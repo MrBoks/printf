8997e0f4498cc7ce36d5d68214206ca6632c39cd
MrBoks <get2noahodogg@yahoo.com>
2022-11-24 13:40:46 +0000

    New

---
+++ valid_args.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * valid_args - check for valid args.
+ * Description: check that % had a valid valid arg
+ * @format: the char * to check
+ * @valist: argument list
+ * Return: void
+ **/
+int valid_args(const char **format, va_list valist)
+{
+	int i, printed;
+
+	op_t ops[] = {
+		{"c", p_char},
+		{"s", p_string},
+		{"i", p_int},
+		{"d", p_d_int},
+		{"b", p_bin},
+		{"u", p_uint},
+		{"o", p_oct},
+		{"x", p_x_min},
+		{"X", p_x_may},
+		{NULL, NULL}
+	};
+	printed = 0;
+
+	for (i = 0; *(ops[i].f) != NULL; i++)
+	{
+		if (*format[0] == '%')
+		{
+			_putchar('%');
+			printed++;
+			break;
+		}
+		else if (*format[0] == *(ops[i].op))
+		{
+			printed += ops[i].f(valist);
+			break;
+		}
+	}
+
+	if (*(ops[i].f) == NULL)
+	{
+		_putchar('%');
+		_putchar(*format[0]);
+		printed += 2;
+	}
+	return (printed);
+}
